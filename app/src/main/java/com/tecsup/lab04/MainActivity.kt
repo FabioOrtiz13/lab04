93b9502b18f4fe61da8b00817ab23b5ba01ce0f6
Fabio O <fortiz@unsa.edu.pe>
2025-04-13 18:53:42 -0500

    Agregamos componente LazyColum al proyecto

--- app/src/main/java/com/tecsup/lab04/MainActivity.kt
+++ app/src/main/java/com/tecsup/lab04/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.tecsup.lab04.ui.theme.Lab04Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab04Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LazyColumnExample(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +31,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LazyColumnExample(modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(5) { index ->
+            Text(
+                text = "Numero: $index",
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Lab04Theme {
-        Greeting("Android")
+        LazyColumnExample()
     }
 }
