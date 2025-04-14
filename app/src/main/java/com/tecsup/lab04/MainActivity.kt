e1b6ab02502dcd63f880093c4aaebeff9beac413
Fabio O <fortiz@unsa.edu.pe>
2025-04-13 19:21:18 -0500

    Agregar componente Image

--- app/src/main/java/com/tecsup/lab04/MainActivity.kt
+++ app/src/main/java/com/tecsup/lab04/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tecsup.lab04.ui.theme.Lab04Theme
@@ -23,7 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab04Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LazyColumnExample(modifier = Modifier.padding(innerPadding))
+                    Imagenn(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,21 +34,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun LazyColumnExample(modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(5) { index ->
-            Text(
-                text = "Numero: $index",
-                modifier = Modifier.padding(16.dp)
-            )
-        }
-    }
+fun Imagenn(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.griz),
+        contentDescription = "imagen",
+        modifier = Modifier.size(200.dp)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Lab04Theme {
-        LazyColumnExample()
+        Imagenn()
     }
 }
